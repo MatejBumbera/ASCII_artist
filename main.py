cea22b0caed81c692bdd7180d1c96dfa1df4b975
Matej Bumbera <matejbumbera1@gmail.com>
2024-08-19 15:37:08 +0200

    added progress bar

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import cv2
 import matplotlib.pyplot as pl
 import sys
 
+
+
+def printProgress(progress):
+    print(f'Progress: |{progress*'#'}{(100-progress)*'-'}| {progress}%', end='\r')
+    if progress == 100:
+        print('')
+
 def overlap(img, origin_x, origin_y, height, width):
     # Determins overlapping based on the top left and 
     # top right corner of the letter we are inserting.
@@ -10,21 +17,22 @@ def overlap(img, origin_x, origin_y, height, width):
            or img[origin_y - height][origin_x] == 0 \
            or img[origin_y - height][origin_x + width] == 0
 
+
 def insertLetter(origin, letter, img):
-    # Inserts letter if it does not overlap and if it fits 
+    # Inserts letter if it does not overlap and if it fits
     # in the picture.
     origin_x, origin_y = origin
     _, img_width = img.shape
-    fontFace = cv2.FONT_HERSHEY_TRIPLEX 
+    fontFace = cv2.FONT_HERSHEY_TRIPLEX
     '''fontFaces:
     FONT_HERSHEY_SCRIPT_SIMPLEX
-    FONT_HERSHEY_SCRIPT_COMPLEX 
+    FONT_HERSHEY_SCRIPT_COMPLEX
     FONT_HERSHEY_COMPLEX_SMALL
-    FONT_HERSHEY_TRIPLEX 
+    FONT_HERSHEY_TRIPLEX
     FONT_HERSHEY_COMPLEX
     FONT_HERSHEY_DUPLEX
     FONT_HERSHEY_PLAIN
-    FONT_HERSHEY_SIMPLEX 
+    FONT_HERSHEY_SIMPLEX
     '''
     fontScale = 1
     'change this value to increase the size of inserted letters'
@@ -40,12 +48,13 @@ def insertLetter(origin, letter, img):
     cv2.rectangle(img, origin, (origin_x + txt_width, origin_y - txt_height), 255, thickness=-1)
     cv2.putText(img, letter, origin, fontFace, fontScale, 0, thickness)
 
-
+progress = 0
+printProgress(progress)
 img = cv2.imread(sys.argv[1], 0)
 img[img != 0] -= 1
 img[img == 0] += 1
 # We want 0 and 255 to be special values, 
-# used to determinoverlapping of letters.
+# used to determin overlapping of letters.
 
 for y, row in enumerate(img):
     for x in range(len(row)):
@@ -61,7 +70,12 @@ for y, row in enumerate(img):
             insertLetter((x, y), 'M', img)
         elif row[x] >= 0:
             insertLetter((x, y), 'W', img)
-"change letters in '' to choose what letters you want to insert"
+        "change letters in '' to choose what letters you want to insert"
+
+    partial_progress = (100*y)//(len(img)-1)
+    if partial_progress > progress:
+        progress = partial_progress
+        printProgress(progress)
 
 img[img != 0] = 255
 
